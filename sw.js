bad35baff60d01ad2844e98d24acc839eba8652f
nhirsama <nhirsama@outlook.com>
2025-10-06 01:09:45 +0000

    Deploy: Merge pull request #6 from nhirsama/dependabot/npm_and_yarn/hexo-theme-reimu-1.10.0
    Bump hexo-theme-reimu from 1.9.2 to 1.10.0 68c12174a770f7cd4257020c89ce5027acd86c0a

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759680575798";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759712983761";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
