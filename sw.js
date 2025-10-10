6a294961c0371b7bcec95300c459475c9836b3e2
nhirsama <nhirsama@outlook.com>
2025-10-10 08:25:36 +0000

    Deploy: 新增网页版GPGTool 308fd7f9845ffb3e92c4381aec434dac3458460e

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759712983761";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760084735766";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
