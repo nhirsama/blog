519a5812b363a8fbe40e1f18ec521554d9a20402
nhirsama <nhirsama@outlook.com>
2025-11-01 02:36:15 +0000

    Deploy: Merge pull request #10 from nhirsama/dependabot/npm_and_yarn/hexo-8.1.0
    Bump hexo from 8.0.0 to 8.1.0 ed29e5033a7f97cad962551c056a10913ef1779a

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761194008723";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1761964574836";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
